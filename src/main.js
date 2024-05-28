86e9144c4f39bdf9be7b2670c245697170038832
lingxiao <3280919460@qq.com>
2024-05-29 00:26:22 +0800

    02

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import Stats from 'three/addons/libs/stats.module.js';
 import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
+import * as THREE from 'three';
+
+			import { StereoEffect } from 'three/addons/effects/StereoEffect.js';
 
 // 全局变量
 let camera,
@@ -11,28 +14,40 @@ let camera,
   stats,
   gui,
   settings = {};
-  
-// 初始化场景
-function initScene() {}
 
-function initCamera() {}
+// 初始化场景
+function initScene() {
+  
+
+  
+}
+
+function initCamera() {
+
+}
 
 function initLight(params) {}
 
-function initRenderer(params) {}
+function initRenderer(params) {
 
+}
+
+function onDocumentMouseMove( event ) {
+
+}
 // requestAnimationFrame  |  setAnimationLoop
-function animate() {}
+function animate() {
+
+}
 
 function onWindowResize() {
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  camera.aspect = window.innerWidth / window.innerHeight;
-  // 如果相机的一些属性发生了变化，需要执行updateProjectionMatrix ()方法更新相机的投影矩阵
-  camera.updateProjectionMatrix();
+
 }
 
 // 坐标系辅助对象、创建轨道控制器、创建stats对象 等辅助工具
-function initHelper() {}
+function initHelper() {
+
+}
 
 function initGUI() {
   gui = new GUI();
